b6becd537bcc966225f270606e92c8d9632231fd
IkeyBenz <ikey.benz@gmail.com>
2025-07-07 16:03:30 -0400

    fixed icons

--- ui/elements/Icon/index.tsx
+++ ui/elements/Icon/index.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { Platform } from 'react-native';
+import React from "react";
+import { Platform } from "react-native";
 
-import BaseIcon from '@expo/vector-icons/Ionicons';
-import { IconProps as BaseIconProps } from '@expo/vector-icons/build/createIconSet';
+import BaseIcon from "@expo/vector-icons/Ionicons";
+import {
+  IconProps as BaseIconProps,
+  Icon as BaseIconType,
+} from "@expo/vector-icons/build/createIconSet";
 
-
-type IconName = 'back' | 'home';
+type IconName = "back" | "home";
 interface IconProps extends BaseIconProps<IconName> {
   size?: number;
 }
 
+type IoniconIconName = typeof BaseIcon extends BaseIconType<infer G, infer FN>
+  ? G
+  : never;
+
+console.log(Platform.OS);
+
 const iconNameMap = {
-  back: Platform.OS === 'ios' ? 'ios-arrow-back' : 'md-arrow-back',
-  home: 'home-sharp',
-} as const;
+  back: "arrow-back",
+  home: "home-sharp",
+} satisfies Record<IconName, IoniconIconName>;
 
 const Icon: React.FC<IconProps> = ({ name, ...props }) => {
   return <BaseIcon name={iconNameMap[name]} {...props} />;
