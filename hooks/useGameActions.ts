4c2e0c70df0e27cba1ac47020409a8408b2b77e6
IkeyBenz <ikey.benz@gmail.com>
2025-07-18 12:21:03 -0400

    merge private game actions with public game actions in useGameAction

--- hooks/useGameActions.ts
+++ hooks/useGameActions.ts
@@ -1,6 +1,7 @@
 import { firestore } from "@/config/firebase";
 import { CardID } from "@/functions/src/types";
 import { ActionType, DealerDrawAction, GameAction } from "@/functions/src/types/actions.types";
+import { useUserId } from "@/providers/Auth";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { useEffect, useRef } from "react";
 
@@ -17,6 +18,7 @@ interface GameActionHandlers {
 export function useGameActions(gameId: string, handlers: GameActionHandlers) {
   const actionQueue = useRef<GameAction[]>([]).current;
   const processing = useRef(false);
+  const userId = useUserId();
 
   async function processActionQueue() {
     if (processing.current) return;
@@ -31,19 +33,40 @@ export function useGameActions(gameId: string, handlers: GameActionHandlers) {
 
   useEffect(() => {
     const actionsRef = collection(firestore, "games", gameId, "actions");
+    const privateActionsRef = collection(firestore, "games", gameId, "privateActions", userId, "actions");
+
     const actionsQuery = query(actionsRef, orderBy("timestamp"));
-    const unsubscribe = onSnapshot(actionsQuery, (snapshot) => {
+    const privateActionsQuery = query(privateActionsRef, orderBy("timestamp"));
+
+    function addActionToQueue(action: GameAction) {
+      actionQueue.push(action);
+      actionQueue.sort((a, b) => a.timestamp.toMillis() - b.timestamp.toMillis());
+      processActionQueue();
+    }
+
+    const unsubscribePublicActions = onSnapshot(actionsQuery, (snapshot) => {
       snapshot.docChanges().filter((change) => change.type === 'added').forEach((change) => {
         const action = change.doc.data() as GameAction;
-        actionQueue.push(action);
-        processActionQueue();
+        addActionToQueue(action);
       });
     });
-    return () => unsubscribe();
+
+    const unsubscribePrivateActions = onSnapshot(privateActionsQuery, (snapshot) => {
+      snapshot.docChanges().filter((change) => change.type === 'added').forEach((change) => {
+        const action = change.doc.data() as GameAction;
+        addActionToQueue(action);
+      });
+    });
+
+    return () => {
+      unsubscribePublicActions();
+      unsubscribePrivateActions();
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [gameId]);
 
   const handleAction = async (action: GameAction) => {
+
     switch (action.type) {
       case ActionType.GAME_STARTED:
         return handlers.moveDeck?.(action.initialDealer);
@@ -63,8 +86,32 @@ export function useGameActions(gameId: string, handlers: GameActionHandlers) {
       case ActionType.END_ROUND:
         return handlers.trashCards?.().then(() => handlers?.moveDeck?.(action.newDealer));
 
-      default:
+      case ActionType.RECEIVE_CARD:
+        return handlers.revealCards?.({ [action.playerId]: action.card });
+
+      case ActionType.DECK_RESHUFFLE:
+        console.warn("Deck reshuffled without action handler", action);
         return Promise.resolve();
+
+      case ActionType.STICK:
+        console.warn("Stick action without action handler", action);
+        return Promise.resolve();
+
+      case ActionType.SPECIAL_EVENT:
+        console.warn("Special event action without action handler", action);
+        return Promise.resolve();
+
+      case ActionType.PLAYER_JOINED:
+        console.warn("Player joined action without action handler", action);
+        return Promise.resolve();
+
+      case ActionType.PLAYER_LEFT:
+        console.warn("Player left action without action handler", action);
+        return Promise.resolve();
+      
+      default:
+        const item: never = action;
+        throw new Error(`Unknown action type: ${item}`);
     }
   };
 } 
