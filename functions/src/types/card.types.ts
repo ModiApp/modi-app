b79e65cf10f6c322bd62b4110e33ce546a06dc78
IkeyBenz <ikey.benz@gmail.com>
2025-08-09 23:03:06 -0400

    cloud functions are a rip off part 3

--- functions/src/types/card.types.ts
+++
@@ -1,3 +0,0 @@
-export type CardRank = '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K' | 'A';
-export type CardSuit = 'H' | 'D' | 'C' | 'S';
-export type CardID = `${CardRank}${CardSuit}`;
